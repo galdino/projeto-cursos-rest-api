c815beeb8eaf88917943a0e77df349fb483c6a59
Galdino <fgaldinoo@gmail.com>
2018-06-28 22:10:12 -0300

    Created class DetalheErro

--- src/main/java/com/galdino/cursos/model/DetalheErro.java
+++ src/main/java/com/galdino/cursos/model/DetalheErro.java
@@ -2,6 +2,8 @@ package com.galdino.cursos.model;
 
 import java.io.Serializable;
 
+import org.springframework.http.HttpStatus;
+
 public class DetalheErro implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -40,6 +42,50 @@ public class DetalheErro implements Serializable {
 
 	public String getPath() {
 		return path;
+	}
+	
+	public static Builder builder() {
+		return new Builder();
+	}
+	
+	public static class Builder{
+		
+		private DetalheErro erro;
+		
+		Builder() {
+			this.erro = new DetalheErro();			
+		}
+		
+		public Builder addStatus(HttpStatus status) {
+			this.erro.statusCode = status.value();
+			this.erro.statusMessage = status.getReasonPhrase();
+			return this;
+		}
+		
+		public Builder addHttpMethod(String method) {
+			this.erro.httpMethod = method;
+			return this;
+		}
+		
+		public Builder addErro(String erro) {
+			this.erro.erro = erro;
+			return this;
+		}
+		
+		public Builder addDetalhe(String detalhe) {
+			this.erro.detalhe = detalhe;
+			return this;
+		}
+		
+		public Builder addPath(String path) {
+			this.erro.path = path;
+			return this;
+		}
+		
+		public DetalheErro build() {
+			return this.erro;
+		}
+		
 	}
 
 }
